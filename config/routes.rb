6f47db5761513cda88edd2e30b51867393d3b339
robin <robin.j.soto@gmail.com>
2020-09-22 18:21:18 -0700

    creates auth app and user, login, and authentication

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  namespace :api do
+
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+
+  end
+end
